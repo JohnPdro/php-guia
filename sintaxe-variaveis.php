21ce2aa4dae781d9c7edd0524a07f6749d9ff838
João Pedro <112526011+JohnPdro@users.noreply.github.com>
2023-05-24 20:25:05 -0300

    Adição de mais algumas informações

--- sintaxe-variaveis.php
+++ sintaxe-variaveis.php
@@ -4,7 +4,17 @@
     /* comentario de varias linhas */
     
 // A linguagem PHP é Case senssitive - diferencia maiusculas e minusculas
-echo "Olá";
-echo "<h1>PHP - Estudando</h1>";
 
+// formas de exibição echo, print, print_r
+// o mais utilizado é o echo e o print poucas diferenças, já o print_r é utilizado para debugar
+echo "Olá";
+echo "<h1>PHP - Estudando</h1>"; // echo é o mais utilizado
+print("<h3>Aula 02</h3>"); // print tem poucas diferenças do echo
+print_r("Vamos estudar!!!"); // mais utilizado para debugar mas funciona com texto normalmente averá conflito com variaveis
+
+$valor = 10;
+print_r("Vamos estudar!!!");
+
+echo "<h3>Mensagem</h3>";
+echo 10.5;
 ?>
