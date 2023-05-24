b3f7b6d374cc00f1985ff01e92836493e2190e30
João Pedro <112526011+JohnPdro@users.noreply.github.com>
2023-05-24 20:13:47 -0300

    Criando guia

---
+++ sintaxe-variaveis.php
@@ -0,0 +1,10 @@
+<?php
+    //Sintaxe e Variáveis
+    #comentario
+    /* comentario de varias linhas */
+    
+// A linguagem PHP é Case senssitive - diferencia maiusculas e minusculas
+echo "Olá";
+echo "<h1>PHP - Estudando</h1>";
+
+?>
