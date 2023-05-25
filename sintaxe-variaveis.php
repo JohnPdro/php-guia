06522f0026041f795b91214dd4592762e1d7de51
João Pedro <112526011+JohnPdro@users.noreply.github.com>
2023-05-24 21:15:07 -0300

    Adição de informações

--- sintaxe-variaveis.php
+++ sintaxe-variaveis.php
@@ -6,7 +6,6 @@
 // A linguagem PHP é Case senssitive - diferencia maiusculas e minusculas
 
 // formas de exibição echo, print, print_r
-// o mais utilizado é o echo e o print poucas diferenças, já o print_r é utilizado para debugar
 echo "Olá";
 echo "<h1>PHP - Estudando</h1>"; // echo é o mais utilizado
 print("<h3>Aula 02</h3>"); // print tem poucas diferenças do echo
@@ -15,6 +14,45 @@ print_r("Vamos estudar!!!"); // mais utilizado para debugar mas funciona com tex
 $valor = 10;
 print_r("Vamos estudar!!!");
 
-echo "<h3>Mensagem</h3>";
-echo 10.5;
+echo "<h2>Mensagem</h2>";
+echo 10.5 . "<br>";
+
+
+// Variaveis
+$nome = "Guto"; //texto = string
+$idade = 36; // numerico inteiro = integer
+$altura = 1.75; // numerico decimal = double
+$fumante = false; // booleano = boolean
+
+echo gettype($nome)  . "<br>";
+echo gettype($idade) . "<br>";
+echo gettype($altura) . "<br>";
+echo gettype($fumante) . "<br>";
+
+
+echo $nome;
+echo "<br>";
+echo $nome . $idade;
+echo "<br>";
+echo $nome . " - " . $idade . "<br>"; // O . é utilizado para concatenar 
+echo "Nome: " . $nome . "<br>"; 
+echo "Nome: $nome <br>"; // Aspas duplas interpreta a variavel 
+echo 'Nome: $nome <br>'; // Aspas simple não interpreta a variavel
+
+
+// Operações  
+
+echo "<h1> Operações </h1>";
+$n1 = 10;
+$n2 = 2;
+echo $n1+$n2;
+echo "<br>";
+echo "n1 + n2 = " . $n1+$n2 . "<br>";
+echo $n1 - $n2 . "<br>";
+echo $n1 * $n2 . "<br>";
+echo $n1 / $n2 . "<br>";
+echo $n1 % $n2 . "<br>";
+echo $n1 ** $n2 . "<br>";
+
+
 ?>
