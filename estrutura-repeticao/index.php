4a39b05f6fd58201c4ef85605aa30e2bff52c546
João Pedro <112526011+JohnPdro@users.noreply.github.com>
2023-06-01 19:40:50 -0300

    atualizando aparencia

--- estrutura-repeticao/index.php
+++ estrutura-repeticao/index.php
@@ -8,11 +8,11 @@
     <title>Insta Clone</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+
 </head>
 
 <body>
-    <div class="container-fluid g-0">
-        <h1><i class="bi bi-heart"></i> Funciona</h1>
+    <div class="container-fluid g-1">
         <?php
         $legendas = ["Rio de Janeiro", "Jalapão", "Chapada Diamantina"];
         $fotos = ["rio.jpg", "jalapao.jpg", "diamantina.jpg"];
@@ -36,7 +36,19 @@
                     </div>
                 </div>
                 <img src="img/<?php echo $fotos[$i]; ?>" class="img-fluid" alt="">
-                <p><?php echo $legendas[$i]; ?></p>
+                <div class="row">
+                    <div class="col-12">
+                        <div class="">
+                            <i class="bi bi-heart"></i>
+                            <i class="bi bi-chat"></i>
+                            <i class="bi bi-send"></i>
+                            <i class="bi bi-bookmark"></i></p>
+                        </div>
+                    </div>
+                    <div class="col-12">
+                        <p><?php echo $legendas[$i]; ?></p>
+                    </div>
+                </div>
             </div>
         <?php
         endfor;
